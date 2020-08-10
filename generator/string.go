37b7cd09d19910770e655fcf6a20f77af51da6e6
Daniel Wu <wxc@wxccs.org>
2020-08-10 09:36:58 +0800

    add RandStringRunes

---
+++ generator/string.go
@@ -0,0 +1,16 @@
+package generator
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandStringRunes(n int) string {
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	b := make([]rune, n)
+	rand.Seed(time.Now().UnixNano())
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
